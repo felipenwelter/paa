b5d4c95e2689f6692e231918ccde39af53b9ccce
Mac <mac@MacBook-Pro-de-Mac-2.local>
2019-04-03 20:50:02 -0300

    commit incomplete matrix multiplication

---
+++ list01/5-matrix-multiplication.py
@@ -0,0 +1,11 @@
+# Multiplicação de matrizes
+
+a = [(2,3),(1,0),(4,5)]
+b = [(3,1),(2,4)]
+res = 0
+
+#for x in a:
+#    for y in x:
+#        res = 0
+#        for z in b:
+#            res = res + y*z
